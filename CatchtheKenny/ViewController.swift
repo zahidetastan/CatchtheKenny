71661e56bec47f37ab2a23b9f887cf44f3f7d71c
Zahide Tastan <zahidett60@gmail.com>
2020-10-21 23:12:16 +0300

    Initial Commit

---
+++ CatchtheKenny/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CatchtheKenny
+//
+//  Created by Zahide Tastan on 21.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
